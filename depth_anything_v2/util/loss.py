abd213cfd44247ba6d3944c05cef6402af5a1441
khengari77 <khengari77@gmail.com>
2025-02-01 17:57:07 +0200

    debug

--- depth_anything_v2/util/loss.py
+++ depth_anything_v2/util/loss.py
@@ -8,12 +8,12 @@ class SiLogLoss(nn.Module):
         self.lambd = lambd
 
     def forward(self, pred, target, valid_mask):
-        valid_mask = valid_mask.detach()
-#        height, width = target.shape[-2:]
-#        pred = F.interpolate(pred[:, None], (height, width), mode="bilinear", align_corners=True)[0,0]
         print("pred", pred.shape)
         print("target", target.shape)
         print("valid_mask", valid_mask.shape)
+        valid_mask = valid_mask.detach()
+#        height, width = target.shape[-2:]
+#        pred = F.interpolate(pred[:, None], (height, width), mode="bilinear", align_corners=True)[0,0]
         diff_log = torch.log(target[valid_mask]) - torch.log(pred[valid_mask])
         loss = torch.sqrt(torch.pow(diff_log, 2).mean() -
                           self.lambd * torch.pow(diff_log.mean(), 2))
