bbc0ea937e2feb6b5b1fa60a934d5726595d0083
khengari77 <khengari77@gmail.com>
2025-02-01 18:18:46 +0200

    pred & target sizes should match

--- depth_anything_v2/util/loss.py
+++ depth_anything_v2/util/loss.py
@@ -8,12 +8,10 @@ class SiLogLoss(nn.Module):
         self.lambd = lambd
 
     def forward(self, pred, target, valid_mask):
-        print("pred", pred.shape)
-        print("target", target.shape)
-        print("valid_mask", valid_mask.shape)
+        height, width = valid_mask.shape[-2:]
+        pred = F.interpolate(pred[:, None], (height, width), mode="bilinear", align_corners=True)
+        pred = pred.squeeze(1).unsqueeze(-1)
         valid_mask = valid_mask.detach()
-#        height, width = target.shape[-2:]
-#        pred = F.interpolate(pred[:, None], (height, width), mode="bilinear", align_corners=True)[0,0]
         diff_log = torch.log(target[valid_mask]) - torch.log(pred[valid_mask])
         loss = torch.sqrt(torch.pow(diff_log, 2).mean() -
                           self.lambd * torch.pow(diff_log.mean(), 2))
